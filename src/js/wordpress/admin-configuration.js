57a3520643bece82a306076fa8315e0a1c66a683
Ben Marshall <bmarshall511@gmail.com>
2019-10-16 20:14:51 -0500

    updates

--- src/js/wordpress/admin-configuration.js
+++ src/js/wordpress/admin-configuration.js
@@ -1,10 +1,74 @@
 (function($) {
   $(() => {
-    const $submitConfig       = $('#importConfig'),
-          $configSavingStatus = $('#configSavingStatus'),
-          $configImportStatus = $('#configImportStatus'),
-          $importBlock        = $('#importBlock');
+    const $submitConfig       = $( '#importConfig' ),
+          $configSavingStatus = $( '#configSavingStatus' ),
+          $configImportStatus = $( '#configImportStatus' ),
+          $importBlock        = $( '#importBlock' ),
+          $generateTheme      = $( '#generateChildTheme' ),
+          $childThemeStatus   = $( '#childThemeStatus' );
 
+    // Genearte child theme
+    $generateTheme.click(function(e) {
+      e.preventDefault();
+
+      const $themeName  = $( '#childThemeName' );
+      const $textDomain = $( '#childThemeTextDomain' );
+      const $themeDesc  = $( '#childThemeDesc' );
+      const $themeURL   = $( '#childThemeURL' );
+      const $themeTags  = $( '#childThemeTags' );
+
+      let data = new FormData();
+      data.append( 'action', 'foundation_generate_theme' );
+      data.append( 'theme_name', $themeName.val() );
+      data.append( 'text_domain', $textDomain.val() );
+      data.append( 'theme_desc', $themeDesc.val() );
+      data.append( 'theme_url', $themeURL.val() );
+      data.append( 'theme_tags', $themeTags.val() );
+      data.append( 'security', FOUNDATION.nonce );
+
+      $themeName.prop( 'disabled', true );
+      $textDomain.prop( 'disabled', true );
+      $themeDesc.prop( 'disabled', true );
+      $themeURL.prop( 'disabled', true );
+      $themeTags.prop( 'disabled', true );
+      $generateTheme.prop( 'disabled', true );
+
+      $childThemeStatus
+        .removeClass( 'error' )
+        .removeClass( 'success' )
+        .html('');
+
+      fetch( ajaxurl, {
+        method: 'POST',
+        body: data
+      })
+      .then(response => response.json())
+      .then(response => {
+        $themeName.prop( 'disabled', false );
+        $textDomain.prop( 'disabled', false );
+        $themeDesc.prop( 'disabled', false );
+        $themeURL.prop( 'disabled', false );
+        $themeTags.prop( 'disabled', false );
+        $generateTheme.prop( 'disabled', false );
+
+        if ( response.error.length ) {
+          $childThemeStatus.addClass( 'error' );
+          response.error.forEach(function(errorMsg) {
+            $childThemeStatus.append( '<p><strong>' + errorMsg + '</strong></p>' );
+          });
+        } else if( response.success.length ) {
+          $childThemeStatus.addClass( 'success' );
+        }
+
+        if ( response.success.length ) {
+          response.success.forEach(function(successMsg) {
+            $childThemeStatus.append( '<p><strong>' + successMsg + '</strong></p>' );
+          });
+        }
+      })
+    });
+
+    // Import configuration
     $submitConfig.click(function(e) {
       e.preventDefault();
 
